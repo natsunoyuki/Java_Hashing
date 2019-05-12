92345caa2a2b1a750ada4d3b06a1e0246512239d
natsunoyuki <33706936+natsunoyuki@users.noreply.github.com>
2019-05-12 23:13:55 +0900

    Add files via upload

---
+++ Hashing.java
@@ -0,0 +1,52 @@
+import java.security.NoSuchAlgorithmException; 
+import java.security.MessageDigest; 
+import java.math.BigInteger; 
+
+//In this file, main throws a NoSuchAlgorithmException while SHA256
+//handles the exception using try-catch. This method requires main to
+//explicitly throw an exception, but frees the user from explicitly 
+//using try-catch every time SHA256 is called.
+
+public class Hashing{
+    public static void main(String args[]) throws NoSuchAlgorithmException{ 
+        //For the test string of "Hello, World!", we expect the result:
+        //dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f
+        String string = "Hello, World!"; 
+        String hashedString = SHA256(string, 16, 64);
+        System.out.println(string);
+        System.out.println(hashedString); 
+    } 
+    //NoSuchAlgorithmException is thrown when a particular cryptographic  
+    //algorithm is requested but is not available in the environment.
+    public static String SHA256(String string, int N, int M){ 
+		//if try-catch is placed here, then main must throws Exception.
+        try { 
+            MessageDigest hasher = MessageDigest.getInstance("SHA-256"); 
+            //the input string has to be encoded into bytes first!
+            byte[] hashedbytes = hasher.digest(string.getBytes()); 
+            //bytes have range of -128~127
+            BigInteger toInt = new BigInteger(1, hashedbytes); 
+            //convert the array of hashed bytes to a BigInt object
+            String H = toInt.toString(N); //return a hex string  
+            if(H.length() < M){
+			    H=ZeroString(M-H.length())+H; 
+			}
+            return H; //return a zero padded hex string     
+        } 
+        catch(NoSuchAlgorithmException e){ 
+            System.err.println(e.getMessage()); 
+            return null; 
+        } 
+    }
+    public static String ZeroString(int N){
+		//returns a string of zeros of length N 
+		if(N<0){
+		    return "";
+		}
+		String string = "";
+		for(int i=0;i<N;i++){
+		    string += "0";
+		}
+		return string;
+	}
+}
---
+++ Hashing2.java
@@ -0,0 +1,50 @@
+import java.security.NoSuchAlgorithmException; 
+import java.security.MessageDigest; 
+import java.math.BigInteger;
+
+//In this file, main uses try-catch to catch the NoSuchAlgorithmException
+//thrown by SHA256. This is different method from Hashing.java. This
+//method frees the user from having force functions to throw Exceptions,
+//but requires a try-catch method to be used whenever SHA256 is called.
+
+public class Hashing2{
+    public static void main(String args[]){ 
+        //For the test string of "Hello, World!", we expect the result:
+        //dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f
+        String string = "Hello, World!"; 
+        try{
+            String hashedString = SHA256(string, 16, 64);
+            System.out.println(string);
+            System.out.println(hashedString); 
+		}catch(NoSuchAlgorithmException e){
+		    System.err.println(e.getMessage());
+		}
+    } 
+    //NoSuchAlgorithmException is thrown when a particular cryptographic  
+    //algorithm is requested but is not available in the environment.
+    public static String SHA256(String string, int N, int M) throws NoSuchAlgorithmException{ 
+		//if this throws Exception, then main must use try-catch
+        MessageDigest hasher = MessageDigest.getInstance("SHA-256"); 
+        //the input string has to be encoded into bytes first!
+        byte[] hashedbytes = hasher.digest(string.getBytes()); 
+        //bytes have range of -128~127
+        BigInteger toInt = new BigInteger(1, hashedbytes); 
+        //convert the array of hashed bytes to a BigInt object
+        String H = toInt.toString(N); //return a hex string  
+        if(H.length() < M){
+			H=ZeroString(M-H.length())+H; 
+	    }
+        return H; //return a zero padded hex string     
+    }
+    public static String ZeroString(int N){
+		//returns a string of zeros of length N 
+		if(N<0){
+		    return "";
+		}
+		String string = "";
+		for(int i=0;i<N;i++){
+		    string += "0";
+		}
+		return string;
+	}
+}
